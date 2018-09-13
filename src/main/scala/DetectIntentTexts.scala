d5e641395d7752e79736b391e2bc6c51e69e82a3
yuuri111 <s.harada0501@gmail.com>
2018-09-13 21:39:40 +0900

    wrap detect intent by future

--- src/main/scala/DetectIntentTexts.scala
+++ src/main/scala/DetectIntentTexts.scala
@@ -1,6 +1,12 @@
 import com.google.cloud.dialogflow.v2.TextInput.Builder
 import com.google.cloud.dialogflow.v2._
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
+case class DetectIntentTextsException(message: String) extends Exception
+
 object DetectIntentTexts extends App with DialogSession {
 
   def detectIntentTexts(projectId: String, texts: List[String], sessionId: String, languageCode: String)
@@ -11,12 +17,17 @@ object DetectIntentTexts extends App with DialogSession {
 
         val queryInput: QueryInput = QueryInput.newBuilder().setText(textInput).build()
 
-        val response: DetectIntentResponse = detectIntent(projectId, sessionId, queryInput)
+        val futureResponse: Future[DetectIntentResponse] = Future {
+          detectIntent(projectId, sessionId, queryInput)
+        }
+
+        val response: DetectIntentResponse = Await.result(futureResponse, 3 seconds)
 
         Option(response.getQueryResult()) match {
           case Some(result) => Right(result)
-          case None => throw new Exception("return null")
+          case None => throw new DetectIntentTextsException("detect intent return null")
         }
+
       } catch {
         case e: Throwable => Left(e)
       }
